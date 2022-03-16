f265a3ad8b9732edbc7fc67f34ae5eca97eec8fe
thanhtd32 <53929436+thanhtd32@users.noreply.github.com>
2022-03-16 10:02:29 +0900

    Add files via upload

--- MQTTServer/Constant/Topic.cs
+++ MQTTServer/Constant/Topic.cs
@@ -8,10 +8,10 @@ namespace MQTTServer.Constant
 {
     public static class Topic
     {
-        public static string TOPIC_FORMULA = "/uit/farm/run";
-        public static string TOPIC_FORMULA_DEGREE_PH = "/uit/farm/degreeph";
-        public static string TOPIC_FORMULA_DEGREE_EC = "/uit/farm/degreeec";
-        public static string TOPIC_FORMULA_PRESSURE = "/uit/farm/pressure";
-        public static string TOPIC_FORMULA_PUMP_STATUS = "/uit/farm/pump/status";
+        public static string TOPIC_FORMULA = "/uel/farm/run";
+        public static string TOPIC_FORMULA_DEGREE_PH = "/uel/farm/degreeph";
+        public static string TOPIC_FORMULA_DEGREE_EC = "/uel/farm/degreeec";
+        public static string TOPIC_FORMULA_PRESSURE = "/uel/farm/pressure";
+        public static string TOPIC_FORMULA_PUMP_STATUS = "/uel/farm/pump/status";
     }
 }
