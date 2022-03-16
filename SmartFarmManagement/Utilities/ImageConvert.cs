0af7ba0f15057d044c6de5f23a681f7cb023cfca
thanhtd32 <53929436+thanhtd32@users.noreply.github.com>
2022-03-16 10:06:08 +0900

    Add files via upload

---
+++ SmartFarmManagement/Utilities/ImageConvert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace SmartFarmManagement.Utilities
+{
+    public static class ImageConvert
+    {
+        public static BitmapImage ConvertBase64ToBitmapImage(string base64)
+        {
+            if (string.IsNullOrEmpty(base64))
+            {
+                return null;
+            }
+
+            byte[] binaryData = System.Convert.FromBase64String(base64);
+
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.StreamSource = new MemoryStream(binaryData);
+            bi.EndInit();
+            bi.Freeze();
+
+            return bi;
+        }
+    }
+}
